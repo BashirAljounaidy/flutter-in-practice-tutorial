3f6a913bd70ac841d565ef800399bca54102ed9e
Bashir <bma.963@gmail.com>
2020-02-23 15:52:00 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:emailapp/MessageList.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(EmailApp());
+//google 
+class EmailApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blueGrey, accentColor: Colors.red),
+      home: MessageList(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
